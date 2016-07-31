dcae9ccd4433d04100451738f1bc59a22e420a4e
Eugene Kozlov <nekoeuge@gmail.com>
2016-07-31 14:08:45 +0300

    Add XML helpers.

--- Source/FlexEngine/Resource/XMLHelpers.h
+++ Source/FlexEngine/Resource/XMLHelpers.h
@@ -25,4 +25,24 @@ T GetValue(const XMLElement& elem, const T& defaultValue)
     return elem ? To<T>(elem.GetValue()) : defaultValue;
 }
 
+/// Load XML attribute value into variable if element is not empty.
+template <class T>
+void LoadAttribute(const XMLElement& elem, const String& attributeName, T& variable)
+{
+    const String& value = elem.GetAttribute(attributeName);
+    if (!value.Empty())
+    {
+        variable = To<T>(value);
+    }
+}
+
+/// Get XML attribute value from element or default if attribute does not exist.
+template <class T>
+T GetAttribute(const XMLElement& elem, const String& attributeName, const T& defaultValue)
+{
+    T result = defaultValue;
+    LoadAttribute(elem, attributeName, result);
+    return result;
+}
+
 }
