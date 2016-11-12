14ea9c5598e79e1abc7281c453bbe1936a94208a
Eugene Kozlov <nekoeuge@gmail.com>
2016-11-12 22:15:58 +0300

    Add prototype of NetworkArena scene.

---
+++ Run/RunEditor.bat
@@ -0,0 +1,1 @@
+..\build\msvc2015_dx11\bin\FlexEngine.exe ../Asset/Data/Scripts/Editor.as -w -s -borderless -pp Asset -p Data;CoreData;Architect;Cache
---
+++ Run/RunNetworkArena_d.bat
@@ -0,0 +1,1 @@
+..\build\msvc2015_dx11\bin\FlexEngine_d.exe ..\Asset\Architect\Examples\NetworkArena\Run.as -w -s -x 600 -y 720 -pp Asset -p Data;CoreData;Architect;Cache
