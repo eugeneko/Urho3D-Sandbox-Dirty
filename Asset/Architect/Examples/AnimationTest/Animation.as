068d958a565588d10ff3e943f1098153133ef10e
Eugene Kozlov <nekoeuge@gmail.com>
2016-11-23 08:08:30 +0300

    Add prototype of walk animation blending.

--- Asset/Architect/Examples/AnimationTest/Animation.as
+++ Asset/Architect/Examples/AnimationTest/Animation.as
@@ -6,6 +6,16 @@ class Animator : ScriptObject
         AnimationController@ animController = node.GetComponent("AnimationController");
         animController.Play("Swat_WalkFwd.ani", 0, true, 0.2f);
         animController.SetSpeed("Swat_WalkFwd.ani", 1);
+        animController.SetWeight("Swat_WalkFwd.ani", 1);
+        animController.Play("Swat_WalkBwd.ani", 0, true, 0.2f);
+        animController.SetSpeed("Swat_WalkBwd.ani", 1);
+        animController.SetWeight("Swat_WalkBwd.ani", 0.00001);
+        animController.Play("Swat_WalkLeft.ani", 0, true, 0.2f);
+        animController.SetSpeed("Swat_WalkLeft.ani", 1);
+        animController.SetWeight("Swat_WalkLeft.ani", 0.00001);
+        animController.Play("Swat_WalkRight.ani", 0, true, 0.2f);
+        animController.SetSpeed("Swat_WalkRight.ani", 1);
+        animController.SetWeight("Swat_WalkRight.ani", 0.00001);
         
         if (disableFoot)
         {
--- Source/FlexEngine/Animation/FootAnimation.cpp
+++ Source/FlexEngine/Animation/FootAnimation.cpp
@@ -9,6 +9,7 @@
 #include <Urho3D/Graphics/AnimatedModel.h>
 #include <Urho3D/Graphics/Animation.h>
 #include <Urho3D/Graphics/AnimationState.h>
+#include <Urho3D/Graphics/AnimationController.h>
 #include <Urho3D/IO/Log.h>
 // #include <Urho3D/Graphics/Geometry.h>
 // #include <Urho3D/Graphics/IndexBuffer.h>
@@ -100,8 +101,8 @@ struct FootAnimationKeyFrame
     float time_ = 0.0f;
     /// Heel position.
     Vector3 heelPosition_;
-    /// Knee rotation.
-    float kneeRotation_ = 0.0f;
+    /// Knee rotation. Contains only roll component.
+    Quaternion kneeRotation_;
     /// Fix for thigh rotation.
     Quaternion thighRotationFix_;
     /// Fix for calf rotation.
@@ -156,7 +157,7 @@ struct FootAnimationTrack
         FootAnimationKeyFrame result;
         result.time_ = time;
         result.heelPosition_ = Lerp(keyFrame->heelPosition_, nextKeyFrame->heelPosition_, t);
-        result.kneeRotation_ = Quaternion(keyFrame->kneeRotation_).Slerp(Quaternion(nextKeyFrame->kneeRotation_), t).RollAngle();
+        result.kneeRotation_ = keyFrame->kneeRotation_.Slerp(nextKeyFrame->kneeRotation_, t);
         result.thighRotationFix_ = keyFrame->thighRotationFix_.Slerp(nextKeyFrame->thighRotationFix_, t);
         result.calfRotationFix_ = keyFrame->calfRotationFix_.Slerp(nextKeyFrame->calfRotationFix_, t);
         result.heelRotationLocal_ = keyFrame->heelRotationLocal_.Slerp(nextKeyFrame->heelRotationLocal_, t);
@@ -194,6 +195,20 @@ void AppendTrackTimes(PODVector<float>& result, AnimationTrack& track)
 float AngleSigned(const Vector3& lhs, const Vector3& rhs, const Vector3& base)
 {
     return lhs.Angle(rhs) * (lhs.CrossProduct(rhs).DotProduct(base) < 0 ? 1 : -1);
+}
+
+/// Mix in quaternion to another with specified weight. Returns new weight of result quaternion.
+Quaternion MixQuaternion(Quaternion& lhs, const Quaternion& rhs, float weight, float totalWeight)
+{
+    // Nothing to mix at right
+    if (weight < M_EPSILON)
+        return lhs;
+
+    // Nothing to mix at left
+    if (totalWeight < M_EPSILON)
+        return rhs;
+
+    return lhs.Slerp(rhs, weight / (weight + totalWeight));
 }
 
 /// Get angle of knee rotation.
@@ -278,7 +293,7 @@ void CreateFootAnimationTrack(FootAnimationTrack& track, Model* model, Animation
         // Make new frame
         track.keyFrames_[i].time_ = times[i];
         track.keyFrames_[i].heelPosition_ = heelPosition;
-        track.keyFrames_[i].kneeRotation_ = GetKneeRotationAngle(thighPosition, calfPosition, heelPosition, Vector3::FORWARD);
+        track.keyFrames_[i].kneeRotation_ = Quaternion(GetKneeRotationAngle(thighPosition, calfPosition, heelPosition, Vector3::FORWARD));
         track.keyFrames_[i].thighRotationFix_ = thighRotationFix;
         track.keyFrames_[i].calfRotationFix_ = calfRotationFix;
         track.keyFrames_[i].heelRotationLocal_ = heelNode->GetRotation();
@@ -354,8 +369,16 @@ void FootAnimation::PostUpdate(float timeStep)
 
     AnimatedModel* model = node_->GetComponent<AnimatedModel>();
 
-    FootAnimationTrack track;
-    CreateFootAnimationTrack(track, model->GetModel(), animation_, footBoneName_);
+    ResourceCache* cache = GetSubsystem<ResourceCache>();
+    SharedPtr<Animation> animForward(cache->GetResource<Animation>("Swat_WalkFwd.ani"));
+    SharedPtr<Animation> animBackward(cache->GetResource<Animation>("Swat_WalkBwd.ani"));
+    SharedPtr<Animation> animLeft(cache->GetResource<Animation>("Swat_WalkLeft.ani"));
+    SharedPtr<Animation> animRight(cache->GetResource<Animation>("Swat_WalkRight.ani"));
+    FootAnimationTrack trackForward, trackBackward, trackLeft, trackRight;
+    CreateFootAnimationTrack(trackForward, model->GetModel(), animForward, footBoneName_);
+    CreateFootAnimationTrack(trackBackward, model->GetModel(), animBackward, footBoneName_);
+    CreateFootAnimationTrack(trackLeft, model->GetModel(), animLeft, footBoneName_);
+    CreateFootAnimationTrack(trackRight, model->GetModel(), animRight, footBoneName_);
 
     Skeleton& skeleton = model->GetSkeleton();
     Node* thighNode = node_->GetChild(footBoneName_, true);
@@ -369,14 +392,47 @@ void FootAnimation::PostUpdate(float timeStep)
     const float calfLength = (calfNode->GetWorldPosition() - heelNode->GetWorldPosition()).Length();
 
     unsigned frameIndex = 0;
-    const FootAnimationKeyFrame frame = track.SampleFrame(time_, frameIndex);
+    const FootAnimationKeyFrame frameForward = trackForward.SampleFrame(time_, frameIndex);
+    const FootAnimationKeyFrame frameBackward = trackBackward.SampleFrame(time_, frameIndex);
+    const FootAnimationKeyFrame frameLeft = trackLeft.SampleFrame(time_, frameIndex);
+    const FootAnimationKeyFrame frameRight = trackRight.SampleFrame(time_, frameIndex);
+    const Vector3 lookDirection = node_->GetRotation() * Vector3::BACK;
+    Vector4 interpolateFactors;
+    interpolateFactors.x_ = Max(0.0f, -lookDirection.z_);
+    interpolateFactors.y_ = Max(0.0f, lookDirection.z_);
+    interpolateFactors.z_ = Max(0.0f, -lookDirection.x_);
+    interpolateFactors.w_ = Max(0.0f, lookDirection.x_);
+    interpolateFactors /= interpolateFactors.x_ + interpolateFactors.y_ + interpolateFactors.z_ + interpolateFactors.w_;
+    FootAnimationKeyFrame frame;
+    frame.time_ = frameForward.time_;
+    float weight = 0.0f;
+    auto makeMeHappy = [&frame, &weight](const FootAnimationKeyFrame& rhs, float factor)
+    {
+        frame.heelPosition_ += rhs.heelPosition_ * factor;
+        frame.kneeRotation_ = MixQuaternion(frame.kneeRotation_, rhs.kneeRotation_, factor, weight);
+        frame.thighRotationFix_ = MixQuaternion(frame.thighRotationFix_, rhs.thighRotationFix_, factor, weight);
+        frame.calfRotationFix_ = MixQuaternion(frame.calfRotationFix_, rhs.calfRotationFix_, factor, weight);
+        frame.heelRotationLocal_ = MixQuaternion(frame.heelRotationLocal_, rhs.heelRotationLocal_, factor, weight);
+        frame.heelRotationWorld_ = MixQuaternion(frame.heelRotationWorld_, rhs.heelRotationWorld_, factor, weight);
+        weight += factor;
+    };
+    makeMeHappy(frameForward, interpolateFactors.x_);
+    makeMeHappy(frameBackward, interpolateFactors.y_);
+    makeMeHappy(frameLeft, interpolateFactors.z_);
+    makeMeHappy(frameRight, interpolateFactors.w_);
+
+    AnimationController* controller = node_->GetComponent<AnimationController>();
+    controller->SetWeight(animForward->GetName(), Max(0.00001f, interpolateFactors.x_));
+    controller->SetWeight(animBackward->GetName(), Max(0.00001f, interpolateFactors.y_));
+    controller->SetWeight(animLeft->GetName(), Max(0.00001f, interpolateFactors.z_));
+    controller->SetWeight(animRight->GetName(), Max(0.00001f, interpolateFactors.w_));
 
     const Vector3 forwardDirection = node_->GetRotation() * Vector3::FORWARD;
 
     // Resolve foot shape
     const Vector3 newHeelPosition = node_->GetWorldTransform() * (Quaternion(Vector3::UP, groundNormal_) * frame.heelPosition_ + groundOffset_);
     const Vector3 newCalfPosition = ResolveKneePosition(thighNode->GetWorldPosition(), newHeelPosition, forwardDirection,
-        thighLength, calfLength, frame.kneeRotation_);
+        thighLength, calfLength, frame.kneeRotation_.RollAngle());
 
     // Apply foot shape
     if (!MatchChildPosition(*thighNode, *calfNode, newCalfPosition))
@@ -394,7 +450,7 @@ void FootAnimation::PostUpdate(float timeStep)
     heelNode->SetWorldRotation(adjustToGoundRotation * origHeelRotation.Slerp(fixedHeelRotation, adjustFoot_));
 
     thighNode->MarkDirty();
-    time_ = Mod(time_ + timeStep, track.GetLength());
+    time_ = Mod(time_ + timeStep, trackForward.GetLength());
 }
 
 void FootAnimation::SetAnimationAttr(const ResourceRef& value)
