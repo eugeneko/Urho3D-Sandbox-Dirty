ca7d86d775ff43ec11ada8e7532337d8f076da91
Eugene Kozlov <nekoeuge@gmail.com>
2017-03-06 00:48:07 +0300

    Basic Editor integration.

---
+++ EditorSource/Main.cpp
@@ -0,0 +1,13 @@
+// #TODO Move this file into /Source
+
+#include <Urho3DEditor/Application.h>
+
+#include <Urho3D/Core/Main.h>
+
+int Main()
+{
+    Urho3DEditor::Application editor(0, nullptr);
+    return editor.Run();
+}
+
+URHO3D_DEFINE_MAIN(Main());
